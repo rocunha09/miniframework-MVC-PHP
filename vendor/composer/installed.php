eede9c5fd23b7430a0b11bd52259c96bc39791b5
rocunha09 <ro.cunha00)@gmail.com>
2021-04-14 18:45:10 -0300

    criado base do framework, e feito a instalação do composer.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b917c845010703385da02cdb3d0fed1a9f125ebf',
+    'name' => 'vendorname/miniframework',
+  ),
+  'versions' => 
+  array (
+    'vendorname/miniframework' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b917c845010703385da02cdb3d0fed1a9f125ebf',
+    ),
+  ),
+);
